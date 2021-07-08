2e6d25e5b34f9a43f4cc7c77add2ec996a79d305
Gabriel Yoo <47622991+seunghy1468@users.noreply.github.com>
2021-07-08 17:37:52 +0900

    [+] Add vue_practice1

---
+++ vue_practice/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+// el로 div 의 id 혹은 class 지정 가능
+    el: '#app',
+    data: {
+    product: 'Socks',
+    image: "./imgs/vmSocks-green-onWhite.jpg",
+    inventory: 100,
+    inStock: "false"
+    }
+})
